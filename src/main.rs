051d0d76be933b3962a7eec32ad16d35a76777bb
Ramon Brand <ramon2009@gmail.com>
2022-04-20 10:10:17 +0300

    Add parsing of tomls

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,64 @@
+#[macro_use]
+extern crate toml;
+
+use std::fmt::Debug;
+use std::{fs, path::Path};
+
+use toml::Value;
+use walkdir::WalkDir;
+
+macro_rules! debug {
+    ($e:expr) => {
+        println!("{:?}", $e)
+    };
+}
+
+#[derive(Debug)]
+struct PackageInfo {
+    name: String,
+    dependencies: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let paths: Vec<String> = WalkDir::new(".")
+        .into_iter()
+        .map(|w| w.unwrap().path().display().to_string())
+        .collect();
+
+    let toml_file_paths: Vec<String> = paths
+        .iter()
+        .filter(|path| path.ends_with(".toml"))
+        .cloned()
+        .collect();
+
+    let string_tomls: Vec<String> = toml_file_paths
+        .iter()
+        .filter_map(|p| fs::read_to_string(p).ok())
+        .collect();
+
+    let parsed_tomls: Vec<Value> = string_tomls
+        .iter()
+        .filter_map(|t| toml::from_str::<Value>(t.as_str()).ok())
+        .collect();
+
+    let package_infos = parsed_tomls.iter().filter_map(|cargo| {
+        let package_name = cargo
+            .get("package")
+            .and_then(|package| package.get("name"))
+            .and_then(|str| str.as_str());
+        let dependencies = cargo
+            .get("dependencies")
+            .and_then(|value| value.as_table())
+            .and_then(|table| Some(table.keys().cloned().collect()));
+        match (package_name, dependencies) {
+            (Some(_package_name), Some(_dependencies)) => Some(PackageInfo {
+                name: _package_name.to_string(),
+                dependencies: _dependencies,
+            }),
+            _ => None,
+        }
+    });
+
+    debug!(toml_file_paths);
+    debug!(package_infos.collect::<Vec<_>>())
 }
